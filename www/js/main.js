ca97ab08066e106f277c8a790192774e1d3e61d4
MJ <manueljesus@carpioseguros.es>
2014-12-02 21:41:50 +0100

    ya de mueven los coches y colisionan

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,41 @@
 //Variables
 $body = $('body');
 $coche = $('#coche');
+$trafico = $('#trafico');
 $calzada = $('#calzada');
 $calzada.width();
+$calzada.height();
 $coche.position();
 $derecha = $('#derecha');
 $izquierda = $('#izquierda');
 var iLimDer = $calzada.position().left + $calzada.width() - $coche.width();
+var altoCalzada = $calzada.height();
+
+
+
+function crearTrafico() {
+	var posTraficoTop = $trafico.position().top;
+	var posTraficoLeft = $trafico.position().left;
+	var limitInferiorTrafico = $trafico.position().top + $trafico.height();
+	var limittraficoD =  $trafico.position().left + $trafico.width();
+	var limitInferiorCoche = $coche.position().top + $coche.height();	
+	var posCocheTop = $coche.position().top;
+	var posCocheLeft = $coche.position().left;
+	var limitCocheD = $coche.position().left + $coche.width();
+	$trafico.css('display', 'inline-block');
+	$trafico.animate({top: altoCalzada}, 10000);
+	console.log(posTraficoTop + ' ' + posTraficoLeft);
+
+	if (limitInferiorTrafico > posCocheTop && posTraficoTop < limitInferiorCoche 
+		&& posTraficoLeft < limitCocheD && limittraficoD > posCocheLeft) {
+		console.log('Estan a la misma altura......................');
+		$trafico.stop();
+	};
+	if (true) {};
+
+}
+
+setInterval('crearTrafico()', 1);
 
 $izquierda.hover(function() {
 	if ($coche.position().left > 0) {
